86e993d1f0260e826b18acd9ce987a6141d8a066
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2024-06-25 19:06:16 +0200

    ConsoleController + GenreController

---
+++ Sources/App/Controllers/GenreController.swift
@@ -0,0 +1,18 @@
+import Vapor
+import Fluent
+
+struct GenreController: RouteCollection {
+    func boot(routes: any RoutesBuilder) throws {
+        let api = routes.grouped("api", "genres")
+        let jwtSecure = api.grouped(UserPayload.authenticator(),
+                                    UserPayload.guardMiddleware())
+        jwtSecure.get(use: getAllGenres)
+    }
+    
+    @Sendable func getAllGenres(req: Request) async throws -> [Genre] {
+        try await Genre
+            .query(on: req.db)
+            .all()
+    }
+
+}
